8b71cb14ca4864be39aa851776b2fa8c64aff9cd
Mustafa <mustafahafidi96@gmail.com>
2020-01-10 17:44:30 +0100

    Added dataset metadata checks

--- src/data/download_pairs.py
+++ src/data/download_pairs.py
@@ -10,12 +10,12 @@ parser = argparse.ArgumentParser(
     description='This script downloads a dataset containing pairs of (original,repackaged) apk\'s')
 
 
-parser.add_argument("-s", default="pairs.txt",
+parser.add_argument("-s", default="common.txt",
                     help="Source list of pairs")
-parser.add_argument("-l", default=1,
+parser.add_argument("-l",
                     help="Download only first {l} pairs of the source list", type=int)
 args = parser.parse_args()
-
+print "args.l: " + str(args.l)
 sourceFile = args.s
 
 originalDir = "pairs/original"
@@ -38,27 +38,30 @@ originalList = os.listdir(originalDir)
 repackagedList = {}
 
 APIKEY = "a52054307648be3c6b753eb55c093f4c2fa4b03e452f8ed245db653fee146cdd"
-f = open(sourceFile)
+# f = open(sourceFile)
 
-# sys.exit(0)
+# exit(0)
 
-for line in f.readlines()[:args.l]:
+with open(sourceFile) as f:
+    fileLines = f.readlines()
+    linesToDownload = (len(fileLines) if args.l == None else args.l)
+    print "About to download %s pairs" % linesToDownload
+    for line in fileLines[:linesToDownload]:
 
-    line = line.replace("\n", "")
+        line = line.replace("\n", "")
 
-    [SHA256_ORIGINAL, SHA256_REPACKAGE] = line.split(",")
+        [SHA256_ORIGINAL, SHA256_REPACKAGE] = line.split(",")
 
-    apkfile = SHA256_ORIGINAL + ".apk"
+        apkfile = SHA256_ORIGINAL + ".apk"
 
-    if not apkfile in originalList:
-        print "Download apk...."
-        curl_command = 'cd {0} && curl -O --remote-header-name -G -d apikey={1} -d sha256={2} https://androzoo.uni.lu/api/download'
-        cmd = curl_command.format(originalDir,
-                                  APIKEY, SHA256_ORIGINAL)
-        os.system(cmd)
-        cmd = curl_command.format(repackagedDir,
-                                  APIKEY, SHA256_REPACKAGE)
-        os.system(cmd)
+        if not apkfile in originalList:
+            print "Download apk...."
+            curl_command = 'cd {0} && curl -O --remote-header-name -G -d apikey={1} -d sha256={2} https://androzoo.uni.lu/api/download'
+            cmd = curl_command.format(originalDir,
+                                      APIKEY, SHA256_ORIGINAL)
+            os.system(cmd)
+            cmd = curl_command.format(repackagedDir,
+                                      APIKEY, SHA256_REPACKAGE)
+            os.system(cmd)
 
-f.close()
 print "\nDataset up to date.\n"
--- src/main.py
+++ src/main.py
@@ -2,28 +2,135 @@
 import androguard.misc
 import os
 import chalk
+import sys
+from tabulate import tabulate
 
-with open('data/pairs.txt') as f:
-    [original_apk_hash, repackaged_apk_hash] = f.readline().strip().split(',')
-    print chalk.bold("Using first pair of dataset: [%s...],[%s...]") % (
-        original_apk_hash[:10], repackaged_apk_hash[:10])
+print 'Checking existing session'
+SESSION_FILENAME = 'session.ag'
+s = androguard.session.Load(SESSION_FILENAME) if os.path.exists(
+    SESSION_FILENAME) else androguard.session.Session()
 
 
-print chalk.bold('Analyzing original apk:')
-# TODO check if apk exist, if not download
-a1, d1, dx1 = androguard.misc.AnalyzeAPK(
-    "./data/pairs/original/%s.apk" % original_apk_hash)
+def compare_lists(l1, l2, print_difference=True):
+    difference1 = set(l1).difference(l2)
+    difference2 = set(l2).difference(l1)
+    return chalk.bold('%s') % (chalk.red('\nDELETED %s\n---\nADDED %s' % (str(difference1), str(difference2)) if print_difference else 'CHANGED') if len(
+        difference1) != 0 or len(
+        difference2) != 0 else chalk.green("EQUAL"))
 
-a2, d2, dx2 = androguard.misc.AnalyzeAPK(
-    "./data/pairs/repackaged/%s.apk" % repackaged_apk_hash)
 
-print '\nPermissions ORIGINAL:'
-print a1.get_permissions()
+def compare_strings(s1, s2, print_difference=True):
+    return chalk.bold('%s') % (chalk.red('DIFFERENT: %s!=%s' % (s1, s2) if print_difference else 'CHANGED') if s1 != s2 else chalk.green("EQUAL"))
 
-print '\nPermissions REPACKAGED:'
-print a2.get_permissions()
 
-perm_difference = set(a2.get_permissions()).difference(a1.get_permissions())
+analysis_rows = []
 
-print chalk.bold('\nPermissions DIFFERENCE: %s') % (chalk.red(perm_difference) if len(
-    perm_difference) != 0 else chalk.green("EQUAL"))
+with open('data/common.txt') as f:
+    for line in f.readlines():
+        [original_apk_hash, repackaged_apk_hash] = line.strip().split(',')
+        print chalk.bold("Analyzing pair of dataset: [%s...],[%s...]") % (
+            original_apk_hash[:10], repackaged_apk_hash[:10])
+
+        # TODO check if apk exist, if not download
+        a1, d1, dx1 = androguard.misc.AnalyzeAPK(
+            "./data/pairs/original/%s.apk" % original_apk_hash)
+
+        a2, d2, dx2 = androguard.misc.AnalyzeAPK(
+            "./data/pairs/repackaged/%s.apk" % repackaged_apk_hash)
+        # COMPARISONS
+
+        print '\n==============================================\n'
+        print 'Android version code: %s' % compare_strings(
+            a1.get_androidversion_code(), a2.get_androidversion_code())
+
+        print '\nAndroid version name: %s' % compare_strings(
+            a1.get_androidversion_name(), a2.get_androidversion_name())
+
+        print '\nMin SDK version: %s' % compare_strings(
+            a1.get_min_sdk_version(), a2.get_min_sdk_version())
+
+        print '\nMax SDK version: %s' % compare_strings(
+            a1.get_max_sdk_version(), a2.get_max_sdk_version())
+
+        print '\nTarget SDK version: %s' % compare_strings(
+            a1.get_target_sdk_version(), a2.get_target_sdk_version())
+
+        print '\nEffective Target SDK version: %s' % compare_strings(
+            a1.get_effective_target_sdk_version(), a2.get_effective_target_sdk_version())
+        print '\n==============================================\n'
+        print "Permissions: %s" % compare_lists(
+            a1.get_permissions(), a2.get_permissions())
+
+        print '\n==============================================\n'
+        print 'Package name: %s' % compare_strings(
+            a1.get_package(), a2.get_package())
+
+        print '\n==============================================\n'
+        print 'APP name: %s' % compare_strings(
+            a1.get_app_name(), a2.get_app_name())
+
+        print '\n==============================================\n'
+        print 'Activities: %s' % compare_lists(
+            a1.get_activities(), a2.get_activities())
+
+        print '\n==============================================\n'
+        print 'Files: %s' % compare_lists(
+            a1.get_files(), a2.get_files())
+
+        print '\n==============================================\n'
+        print 'Services: %s' % compare_lists(
+            a1.get_services(), a2.get_services())
+
+        print '\n==============================================\n'
+        print 'Receivers: %s' % compare_lists(
+            a1.get_receivers(), a2.get_receivers())
+
+        print '\n========================= CURRENT ANALYSIS =========================\n'
+
+        analysis_rows = analysis_rows + [[
+            compare_strings(
+                a1.get_androidversion_code(), a2.get_androidversion_code(), False),
+            compare_strings(
+                a1.get_androidversion_name(), a2.get_androidversion_name(), False),
+            compare_strings(
+                a1.get_min_sdk_version(), a2.get_min_sdk_version(), False),
+            compare_strings(
+                a1.get_max_sdk_version(), a2.get_max_sdk_version(), False),
+            compare_strings(
+                a1.get_target_sdk_version(), a2.get_target_sdk_version(), False),
+            compare_strings(
+                a1.get_effective_target_sdk_version(), a2.get_effective_target_sdk_version(), False),
+            compare_lists(
+                a1.get_permissions(), a2.get_permissions(), False),
+            compare_strings(
+                a1.get_package(), a2.get_package(), False),
+            compare_strings(
+                a1.get_app_name(), a2.get_app_name(), False),
+            compare_lists(
+                a1.get_activities(), a2.get_activities(), False),
+            compare_lists(
+                a1.get_files(), a2.get_files(), False),
+            compare_lists(
+                a1.get_services(), a2.get_services(), False),
+            compare_lists(
+                a1.get_receivers(), a2.get_receivers(), False)
+        ]]
+        print tabulate(analysis_rows, headers=['And. vcode',
+                                               'And. vname',
+                                               'minsdk',
+                                               'maxsdk',
+                                               'targetsdk',
+                                               'efftarget',
+                                               'permissions',
+                                               'package',
+                                               'appname',
+                                               'activities',
+                                               'Files',
+                                               'Services',
+                                               'Receivers'])
+
+
+# s.save(SESSION_FILENAME)
+# androguard.session.Save(s, SESSION_FILENAME)
+
+# sys.exit(0)
