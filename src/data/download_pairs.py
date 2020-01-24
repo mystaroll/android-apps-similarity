2304bd4735a008c777ce69c707ba4c56c8dd0ce7
Mustafa <mustafahafidi96@gmail.com>
2020-01-24 21:28:30 +0100

    Updated gitlab pipeline and download script

--- src/data/download_pairs.py
+++ src/data/download_pairs.py
@@ -10,18 +10,14 @@ parser = argparse.ArgumentParser(
     description='This script downloads a dataset containing pairs of (original,repackaged) apk\'s')
 
 
-parser.add_argument("-s", default="common.txt",
+parser.add_argument("-s", default="common_with_groundtruth.txt",
                     help="Source list of pairs")
 parser.add_argument("-l",
                     help="Download only first {l} pairs of the source list", type=int)
 args = parser.parse_args()
-print "args.l: " + str(args.l)
 sourceFile = args.s
 
-originalDir = "pairs/original"
-
-repackagedDir = "pairs/repackaged"
-
+originalDir = "apks"
 
 if not os.path.exists(sourceFile):
     print "Can't find source list file. %s nor data/%s exist" % sourceFile
@@ -30,12 +26,7 @@ if not os.path.exists(sourceFile):
 if not os.path.exists(originalDir):
     os.makedirs(originalDir)
 
-if not os.path.exists(repackagedDir):
-    os.makedirs(repackagedDir)
-
 originalList = os.listdir(originalDir)
-
-repackagedList = {}
 
 APIKEY = "a52054307648be3c6b753eb55c093f4c2fa4b03e452f8ed245db653fee146cdd"
 # f = open(sourceFile)
@@ -50,17 +41,16 @@ with open(sourceFile) as f:
 
         line = line.replace("\n", "")
 
-        [SHA256_ORIGINAL, SHA256_REPACKAGE] = line.split(",")
+        [SHA256_ORIGINAL, SHA256_REPACKAGE, _] = line.split(",")
 
-        apkfile = SHA256_ORIGINAL + ".apk"
-
-        if not apkfile in originalList:
+        curl_command = 'cd {0} && curl -O --remote-header-name -G -d apikey={1} -d sha256={2} https://androzoo.uni.lu/api/download'
+        if not (SHA256_ORIGINAL + ".apk") in originalList:
             print "Download apk...."
-            curl_command = 'cd {0} && curl -O --remote-header-name -G -d apikey={1} -d sha256={2} https://androzoo.uni.lu/api/download'
             cmd = curl_command.format(originalDir,
                                       APIKEY, SHA256_ORIGINAL)
             os.system(cmd)
-            cmd = curl_command.format(repackagedDir,
+        if not (SHA256_REPACKAGE + ".apk") in originalList:
+            cmd = curl_command.format(originalDir,
                                       APIKEY, SHA256_REPACKAGE)
             os.system(cmd)
 
